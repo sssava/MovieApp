aaf0a43b5bffd5df0e52430db2861aaf2dc46660
sssava <santa03777@gmail.com>
2022-10-27 00:27:15 +0300

    Full Project

---
+++ movieApp/movie/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class MovieConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'movie'
+
+    def ready(self):
+        import movie.signals
